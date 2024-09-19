7684a65c34c259fc366a225f0f46bebe8c82f022
KmdCode <karabomodise260@gmail.com>
2024-09-19 13:36:12 +0200

    server setup

--- server/app.js
+++ server/app.js
@@ -1,1 +1,33 @@
-const name = "accommodation"
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+
+dotenv.config({path: './config/config.env'})
+const app = express()
+
+app.use(express.json())
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+app.use(cors({
+    origin:'http://localhost:3000',
+    method: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials:true
+}))
+
+app.get('/', (req, res)=>{
+    res.status(200).json({
+        status: 'success',
+        message: 'connection successful'
+    })
+})
+
+const port = process.env.PORT || 8000
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
