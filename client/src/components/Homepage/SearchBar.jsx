4b2ae6b7ab776785d077889a62f68e500d35e6e0
NokubongaCele <202115965@swave.smu.ac.za>
2024-10-11 17:18:10 +0200

    searchbar changed

--- client/src/components/Homepage/SearchBar.jsx
+++ client/src/components/Homepage/SearchBar.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import { useNavigate } from 'react-router-dom';
-
 
 const SearchBar = () => {
   const [minPrice, setMinPrice] = useState('');
@@ -11,45 +10,44 @@ const SearchBar = () => {
 
   const navigate = useNavigate();
 
- const handleSearch = () => {
-  const params = new URLSearchParams({
-    minPrice: minPrice,
-    maxPrice: maxPrice,
-    gender: gender,
-    location: location,
-    isFurnished: isFurnished, 
-  }).toString(); 
-  
-  navigate(`/search-results?${params}`);
-};
-
+  const handleSearch = () => {
+    const params = new URLSearchParams({
+      minPrice: minPrice,
+      maxPrice: maxPrice,
+      gender: gender,
+      location: location,
+      isFurnished: isFurnished, 
+    }).toString(); 
+    
+    navigate(`/search-results?${params}`);
+  };
 
   return (
     <div className="relative w-full">
       <img
-      src="bg2.jpg"
-      alt="Background"
-      className="absolute inset-0 w-full h-full object-cover"
-    />
+        src="bg2.jpg"
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
       <div className="absolute inset-0 bg-black opacity-50"></div>
 
       <div className="relative container mx-auto p-4 max-w-3xl mt-0">
         <div className="bg-white p-6 rounded-md shadow-md">
           <h2 className="text-lg font-semibold mb-4">Search for Properties</h2>
           <div className="flex flex-wrap -mx-2 mb-4">
-          <div className="w-full sm:w-1/2 px-2 mb-4 sm:mb-0">
-            <label htmlFor="minPrice" className="block text-sm font-medium text-gray-700">
-              Min Price
-            </label>
-            <input
-              type="number"
-              id="minPrice"
-              value={minPrice}
-              onChange={(e) => setMinPrice(e.target.value)}
-              className="mt-1 pt-1 pb-1 pl-2 block w-full border-2 border-indigo-600 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              placeholder="Enter minimum price"
-            />
-          </div>
+            <div className="w-full sm:w-1/2 px-2 mb-4 sm:mb-0">
+              <label htmlFor="minPrice" className="block text-sm font-medium text-gray-700">
+                Min Price
+              </label>
+              <input
+                type="number"
+                id="minPrice"
+                value={minPrice}
+                onChange={(e) => setMinPrice(e.target.value)}
+                className="mt-1 pt-1 pb-1 pl-2 block w-full border-2 border-indigo-600 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                placeholder="Enter minimum price"
+              />
+            </div>
 
             <div className="w-full sm:w-1/2 px-2">
               <label htmlFor="maxPrice" className="block text-sm font-medium text-gray-700">
@@ -76,11 +74,10 @@ const SearchBar = () => {
                 value={gender}
                 onChange={(e) => setGender(e.target.value)}
                 className="mt-1 pt-1 pb-1 block w-full border-2 border-indigo-600 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                >
-                <option value="">Select Gender</option>
+              >
+                <option value="">Any</option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
-                <option value="any">Any</option>
               </select>
             </div>
 
@@ -88,14 +85,18 @@ const SearchBar = () => {
               <label htmlFor="location" className="block text-sm font-medium text-gray-700">
                 Location
               </label>
-              <input
-                type="text"
+              <select
                 id="location"
                 value={location}
                 onChange={(e) => setLocation(e.target.value)}
-                className="mt-1 pt-1 pb-1 pl-2 block w-full border-2 border-indigo-600 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                placeholder="Enter location"
-              />
+                className="mt-1 pt-1 pb-1 block w-full border-2 border-indigo-600 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              >
+                <option value="">Any</option>
+                <option value="Hatfield">Hatfield</option>
+                <option value="Sunnyside">Sunnyside</option>
+                <option value="Arcadia">Arcadia</option>
+                <option value="Pretoria CBD">Pretoria CBD</option>
+              </select>
             </div>
           </div>
 
