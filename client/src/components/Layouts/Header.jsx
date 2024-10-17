745dad548d5c8019d51413fe26917cae8d87e100
Phagane <matlakala.rp@gmail.com>
2024-10-17 22:23:57 +0200

    layout

--- client/src/components/Layouts/Header.jsx
+++ client/src/components/Layouts/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';//part of the layout
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
