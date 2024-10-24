d657a5a6b50413ccc8e745212946ab8b92f3816b
Phagane <matlakala.rp@gmail.com>
2024-10-25 00:25:37 +0200

    chanes on Admin

--- client/src/components/Admin/LandlordProperties.jsx
+++ client/src/components/Admin/LandlordProperties.jsx
@@ -44,7 +44,7 @@ const LandlordProperties = ({ adminEmail }) => {
                 <p className="text-gray-500 mb-2">Furnished: {property.furnished ? 'Yes' : 'No'}</p>
                 <button
                   className="bg-violet-700 text-white py-2 px-4 rounded-lg hover:bg-black focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 mt-2"
-                  onClick={() => handleManageClick(property.id)}
+                  onClick={() => handleManageOnClick(property.id)}
                 >
                   Manage
                 </button>
