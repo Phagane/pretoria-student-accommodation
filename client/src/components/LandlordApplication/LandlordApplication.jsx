570626fc6e20438b90c50e1a5556ce24b29ccde7
Phagane <matlakala.rp@gmail.com>
2024-10-25 00:21:01 +0200

    chanes on landlords

--- client/src/components/LandlordApplication/LandlordApplication.jsx
+++ client/src/components/LandlordApplication/LandlordApplication.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-
+const LandlordApplication = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-center mb-10">Apply To Become A Landlord</h1>
+      <button></button>
 
 const LandlordApplication = () => {
   return (
